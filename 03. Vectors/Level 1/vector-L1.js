075b072f8567acb1406d0d7a0dd096097997fb91
Dennis Dang <dangggdennis@gmail.com>
2017-12-11 20:40:55 -0800

    Incomplete vector l2

--- 03. Vectors/Level 1/vector-L1.js
+++ 03. Vectors/Level 1/vector-L1.js
@@ -95,17 +95,26 @@ var Vector = function(initialCapacity) {
   this.capacity = initialCapacity || 8;   // Default array size initially to 8 elements
   this.length = 0;
 
-  this.storage = [];
+  this.storage = new Array(this.capacity);
 };
 
 
 Vector.prototype.insert = function(index, value) {
+  if(this.length >= this.capacity) {
+    this.resize();
+  }
   this.storage.splice(index, 0, value);
+  this.length++;
 };
 
 
 Vector.prototype.add = function(value) {
+  // 
+  if(this.length >= this.capacity) {
+    this.resize();
+  }
   this.storage[this.length++] = value;
+
 };
 
 
@@ -116,6 +125,7 @@ Vector.prototype.remove = function(index) {
   else {
     this.storage.splice(index, 1);
   }
+  this.length--;
 };
 
 
@@ -135,6 +145,13 @@ Vector.prototype.set = function(index, value) {
 
 Vector.prototype.resize = function() {
   // ...
+  this.capacity *= 2;
+  var a = new Array(this.capacity);
+  for(let i = 0; i < a.length; i++) {
+    a[i] = this.storage[i];
+  }
+  this.storage = a;
+  // delete a;
 };
 
 
@@ -169,7 +186,7 @@ test(true, function() {
     console.log("  v.toArray() should be [0, 1, 2]: " + (v.toArray().equals([0, 1, 2])));
   });
 
-  test(false, function() {
+  test(true, function() {
     console.log("Add 2 more");
     v.add(3);
     v.add(4);
@@ -184,20 +201,20 @@ test(true, function() {
     console.log("  v.toArray() should be [0, 1, 2, 2.5, 3, 4]: " + (v.toArray().equals([0, 1, 2, 2.5, 3, 4])));
   });
 
-  test(false, function() {
+  test(true, function() {
     console.log("Remove v[3]");
     v.remove(3);
     console.log("  v.length should be 5: " + (v.length === 5));
     console.log("  v.toArray() should be [0, 1, 2, 3, 4]: " + (v.toArray().equals([0, 1, 2, 3, 4])));
   });
 
-  test(false, function() {
+  test(true, function() {
     console.log("Set v[2] = 15");
     v.set(2, 15);
     console.log("  v.get(2) should be 15: " + (v.get(2) === 15));
   });
 
-  test(false, function() {
+  test(true, function() {
     console.log("Add 3 more");
     v.add(5);
     v.add(6);
@@ -206,26 +223,26 @@ test(true, function() {
     console.log("  v.capacity should be 8: " + (v.capacity === 8));
   });
 
-  test(false, function() {
+  test(true, function() {
     console.log("Add 1 more to fill capacity");
     v.add(8);
     console.log("  v.length should be 9: " + (v.length === 9));
     console.log("  v.capacity should be 16: " + (v.capacity === 16));
   });
 
-  test(false, function() {
+  test(true, function() {
     console.log("Remove from the end");
     v.remove();
     console.log("  v.toArray() should be [0, 1, 15, 3, 4, 5, 6, 7]: " + (v.toArray().equals([0, 1, 15, 3, 4, 5, 6, 7])));
   });
 
-  test(false, function() {
+  test(true, function() {
     console.log("Remove v[2]");
     v.remove(2);
     console.log("  v.toArray() should be [0, 1, 3, 4, 5, 6, 7]: " + (v.toArray().equals([0, 1, 3, 4, 5, 6, 7])));
   });
 
-  test(false, function() {
+  test(true, function() {
     console.log("Remove the first");
     v.remove(0);
     console.log("  v.toArray() should be [1, 3, 4, 5, 6, 7]: " + (v.toArray().equals([1, 3, 4, 5, 6, 7])));
@@ -233,13 +250,13 @@ test(true, function() {
     console.log("  v.capacity should be 8: " + (v.capacity === 16));
   });
 
-  test(false, function() {
+  test(true, function() {
     console.log("Insert one at the beginning");
     v.insert(0, 0);
     console.log("  Insert 0 at v[0] should be [0, 1, 3, 4, 5, 6, 7]: " + (v.toArray().equals([0, 1, 3, 4, 5, 6, 7])));
   });
 
-  test(false, function() {
+  test(true, function() {
     console.log("Remove from beginning");
     v.remove(0);
     console.log("  v.remove(0) should be [1, 3, 4, 5, 6, 7]: " + v.toArray().equals([1, 3, 4, 5, 6, 7]));
@@ -247,7 +264,7 @@ test(true, function() {
 
   v = new Vector();
 
-  test(false, function() {
+  test(true, function() {
     console.log("Test inserting <capacity> items leaves the storage size at <capacity>");
     console.log("  Re-Initialize");
     console.log("    v.length should be 0: " + (v.length === 0));
@@ -255,7 +272,7 @@ test(true, function() {
     console.log("    v.storage should be [undefined, ... x8]: " + (v.storage.length === v.capacity));
   });
 
-  test(false, function() {
+  test(true, function() {
     console.log("  Add 6");
     v.add(0);
     v.add(1);
@@ -267,13 +284,13 @@ test(true, function() {
     console.log("    v.toArray() should be [0, 1, 2, 3, 4, 5]: " + (v.toArray().equals([0, 1, 2, 3, 4, 5])));
   });
 
-  test(false, function() {
+  test(true, function() {
     console.log("  Insert 1");
     v.insert(1, 6);
     console.log("    v.toArray() should be [0, 6, 1, 2, 3, 4, 5]: " + (v.toArray().equals([0, 6, 1, 2, 3, 4, 5])));
   });
 
-  test(false, function() {
+  test(true, function() {
     console.log("  Insert 1 More");
     v.insert(1, 7);
     console.log("    v.length should be 8: " + (v.length === 8));
@@ -281,7 +298,7 @@ test(true, function() {
     console.log("    v.toArray() should be [0, 7, 6, 1, 2, 3, 4, 5]: " + (v.toArray().equals([0, 7, 6, 1, 2, 3, 4, 5])));
   });
 
-  test(false, function() {
+  test(true, function() {
     console.log("  Insert 1 Beyond Initial Capactity of 8");
     v.insert(6, 8);
     console.log("    v.length should be 9: " + (v.length === 9));
