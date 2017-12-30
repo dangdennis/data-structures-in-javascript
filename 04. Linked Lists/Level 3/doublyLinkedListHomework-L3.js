094fd00b3d447e9e80a1e5a1614445d43822ffe2
Shawn <shawn@ievolved.io>
2017-12-29 19:57:04 -0800

    [update] Added doublyLinkedList Homework

---
+++ 04. Linked Lists/Level 3/doublyLinkedListHomework-L3.js
@@ -0,0 +1,21 @@
+
+/*
+
+1. Analyze your Vector implementation,  what is the Big-O of each function.  Why?
+2. Analyze your Doubly Linked List implementation, what is the Big-O of each function.  Why?
+3. Write a function using a doubly linked list to return the index of the nth odd number from the tail of the list (think back to the arrays exercise asking the same thing)
+4. Implement a Vector class, where the `this.storage = new DoublyLinkedList()` instead of array like before
+5. Analyze again the Big-O of all the fuctions of the new Vector class using doubly linked list, what is the big-O,why?
+
+Learning Objectives:
+
+1. To understand how different data structures can solve the same purpose
+2. To understand how using different data structures effects the complexit (Big-O)
+3. To make sure that you understand how to use the Doubly Linked List in practice
+
+Reason Why:
+
+ * In interviews, you might have to mix and match data structures so I want you comfortable with it
+
+
+ */
